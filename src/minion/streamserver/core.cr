b1fe9771e993d420d85fbfc225af0988ad03e1aa
J. Austin Hughey <jah@jah.io>
2020-07-07 12:50:13 -0600

    fix malformed array literal bug

--- src/minion/streamserver/core.cr
+++ src/minion/streamserver/core.cr
@@ -212,7 +212,7 @@ module Minion
               else
                 sql = <<-ESQL
                 UPDATE servers
-                SET addresses = addresses || $1, updated_at = now()
+                SET addresses = array_append(addresses, $1), updated_at = now()
                 WHERE id = $2
                 ESQL
                 cnn.exec(sql, agent_address, server_id)
