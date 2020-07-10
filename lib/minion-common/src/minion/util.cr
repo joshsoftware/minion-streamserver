b8a7ba45837fd534276c758b8dc99904db098faf
Kirk Haines <wyhaines@gmail.com>
2020-07-10 13:24:57 -0600

    common code update

--- lib/minion-common/src/minion/util.cr
+++ lib/minion-common/src/minion/util.cr
@@ -1,5 +1,5 @@
 module Minion
-  class Util
+  module Util
     # Given an array or a string, return a string.
     def string_from_string_or_array(val) : String
       val.is_a?(Array) ? val.as(Array).join : val.as(String)
@@ -7,7 +7,7 @@ module Minion
 
     # Let it work if the method is used as a class method.
     def self.string_from_string_or_array(val)
-      self.new.string_from_string_or_array(val)
+      val.is_a?(Array) ? val.as(Array).join : val.as(String)
     end
   end
 end
